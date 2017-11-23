f3ea17e26fd3888b0fd1d4cb98b849f4277eb25d
TheReaper666 <jack0810@edu.campusvejle.dk>
2017-11-23 14:22:38 +0100

    Alt tilbage som det var

---
+++ LoginSystem/Bizz/BizzFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bizz
+{
+    class BizzFunctions
+    {
+        public ObservableCollection<LoginInfo> CollectionOfLogins = new ObservableCollection<LoginInfo>();
+        public ObservableCollection<UserInfo> CollectionOfUsers = new ObservableCollection<UserInfo>();
+        public BizzFunctions()
+        {
+
+        }
+        public bool CheckCredentials(string Username, string Password)
+        {
+            return true;
+        }
+    }
+}
---
+++ LoginSystem/Bizz/UserInfo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bizz
+{
+    class UserInfo
+    {
+        private int id;
+        private string name;
+        private string addresse;
+        private string email;
+        private int phone;
+        private Groupflags groupflag;
+        public UserInfo(string name, string addresse, string email, int phone, Groupflags groupflag)
+        {
+            Name = name;
+            Addresse = addresse;
+            Email = email;
+            Phone = phone;
+            Groupflag = groupflag;
+        }
+        public UserInfo(int id, string name, string addresse, string email, int phone, Groupflags groupflag)
+        {
+            this.id = id;
+            Name = name;
+            Addresse = addresse;
+            Email = email;
+            Phone = phone;
+            Groupflag = groupflag;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Addresse
+        {
+            get
+            {
+                return addresse;
+            }
+            set
+            {
+                addresse = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public int Phone
+        {
+            get
+            {
+                return Phone;
+            }
+            set
+            {
+                Phone = value;
+            }
+        }
+        public Groupflags Groupflag
+        {
+            get
+            {
+                return groupflag;
+            }
+            set
+            {
+                groupflag = value;
+            }
+        }
+        public enum Groupflags { Administrator, Staff, User, Guest }
+    }
+}
