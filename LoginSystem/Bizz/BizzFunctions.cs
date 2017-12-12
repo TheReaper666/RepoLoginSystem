ce95d102476e3cd4a1208a2ff8515d28d1c0bc1d
TheReaper666 <jack0810@edu.campusvejle.dk>
2017-12-12 15:33:37 +0100

    Done for today

--- LoginSystem/Bizz/BizzFunctions.cs
+++ LoginSystem/Bizz/BizzFunctions.cs
@@ -13,8 +13,9 @@ namespace Bizz
 {
     public class BizzFunctions
     {
-        ObservableCollection<Users> fisk;
-        List<Users> ListUsers = new List<Users>();
+        ObservableCollection<Users> OCUsers;
+        ObservableCollection<Logins> OCLogins;
+        ObservableCollection<UsernamesAndNames> OCNamesAndUsernames;
         DBInfo DBinfo;
         public BizzFunctions()
         {
@@ -24,9 +25,10 @@ namespace Bizz
         {
             return true;
         }
-        public List<Users> GetUsersConverted()
+        public ObservableCollection<Users> GetAllUsersConverted()
         {
             DataTable dt = DBinfo.DTGetAllUsersFromDB();
+            OCUsers = new ObservableCollection<Users>();
             DataTableReader reader = new DataTableReader(dt);
             while (reader.Read())
             {
@@ -35,10 +37,43 @@ namespace Bizz
                 string email = reader["Email"].ToString();
                 int phone = Convert.ToInt32(reader["Phone"]);
                 Users user = new Users(name,address,email,phone);
-                ListUsers.Add(user);
+                OCUsers.Add(user);
             }
-            return ListUsers;
+            return OCUsers;
         }
+        public ObservableCollection<Users> GetAllLoginsConverted()
+        {
+            DataTable dt = DBinfo.DTGetAllLoginsFromDB();
+            OCLogins = new ObservableCollection<Logins>();
+            DataTableReader reader = new DataTableReader(dt);
+            while (reader.Read())
+            {
+                string username = reader["Username"].ToString();
+                string password = reader["Password"].ToString();
+                string group = reader["GroupFlag"].ToString();
+                int phone = Convert.ToInt32(reader["Phone"]);
+                Logins login = new Logins(username, password, group);
+                OCLogins.Add(login);
+            }
+            return OCUsers;
+        }
+        public ObservableCollection<UsernamesAndNames> GetAllUsernamesAndNamesConverted()
+        {
+            DataTable dt = DBinfo.DTGetAllUsernamesAndNames();
+            OCNamesAndUsernames = new ObservableCollection<UsernamesAndNames>();
+            DataTableReader reader = new DataTableReader(dt);
+            while (reader.Read())
+            {
+                string username = reader["Username"].ToString();
+                string name = reader["Name"].ToString();
+                bool status = Convert.ToBoolean(reader["status"]);
+                UsernamesAndNames Person = new UsernamesAndNames(username, name, status);
+                OCNamesAndUsernames.Add(Person);
+            }
+            return OCNamesAndUsernames;
+        }
+
+        // SELECT LoginInfo.Username,UserInfo.Name FROM LoginInfo INNER JOIN UserInfo ON LoginInfo.id = UserInfo.id
         // Metoder der kalder andrer metoder i DBInfo
 
         // Metoder der Convertere DataTable og sender data'en til Entities
